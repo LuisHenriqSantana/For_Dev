c5b334de8f6f55e79ed2fb7b3da0f664aaf99062
Luis <luis.henrique8000@gmail.com>
2021-03-31 19:21:32 -0300

    move SUT creation to te setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -4,34 +4,47 @@ import 'package:mockito/mockito.dart';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
-
-class HttpAdapter{
+class HttpAdapter {
   final Client client;
-
   HttpAdapter(this.client);
 
   Future<void> request({
     @required String url,
     @required String method,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(url, headers: headers);
   }
 }
 
-class ClientSpy extends Mock implements Client{}
+class ClientSpy extends Mock implements Client {}
 
-void main (){
-  group('post', (){
-    test('Should call post with correct values', () async{
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+void main() {
+  ClientSpy client;
+  HttpAdapter sut;
+  String url;
 
-      await sut.request(url: url, method: 'post');
-
-      verify(client.post(url));
-
-    });
+  setUp((){
+     client = ClientSpy();
+     sut = HttpAdapter(client);
+     url = faker.internet.httpUrl();
   });
 
+
+  group('post', () {
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        url,
+        headers:{
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
+    });
+  });
 }
