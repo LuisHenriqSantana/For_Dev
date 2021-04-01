78a3ca2eddc03d2c94120def9f84694db931ba53
Luis <luis.henrique8000@gmail.com>
2021-04-01 15:20:59 -0300

    refactor: add ! in throw exception error

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,17 +1,16 @@
 import 'package:for_dev/data/http/http_error.dart';
 import 'package:for_dev/domain/entities/account_entity.dart';
 
-class RemoteAccountModel{
+class RemoteAccountModel {
   final String accessToken;
 
   RemoteAccountModel(this.accessToken);
 
   factory RemoteAccountModel.fromJson(Map json) {
-   if (json.containsKey('accessToken')) {
-     throw HttpError.invalidData;
-   }
+    if (!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
     return RemoteAccountModel(json['accessToken']);
   }
   AccountEntity toEntity() => AccountEntity(accessToken);
-
 }
