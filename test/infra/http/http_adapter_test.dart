7b3a5a83d357029396d2473ebc587939d93e20b9
Luis <luis.henrique8000@gmail.com>
2021-04-01 19:00:32 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -80,7 +80,7 @@ void main() {
       expect(response, null);
     });
 
-    test('Should return BadRequestError  if post returns 400', () async {
+    test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
@@ -88,12 +88,20 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should return BadRequestError  if post returns 400', () async {
+    test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return ServerError if post returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
