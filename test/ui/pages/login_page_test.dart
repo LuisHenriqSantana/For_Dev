3f82c41bd61198d7872f6f2acca42c00edf15daa
Luis <luis.henrique8000@gmail.com>
2021-04-05 19:36:12 -0300

    ensure LoginPage presents no error if email is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -37,7 +37,7 @@ void main() {
     final passwordTextChildren = find.descendant(
         of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
     expect(
-      emailTextChildren,
+      passwordTextChildren,
       findsOneWidget,
     );
 
@@ -66,5 +66,32 @@ void main() {
 
    expect(find.text('any error'), findsOneWidget);
 
+  });
+
+  testWidgets('Should present no error if email is valid', (WidgetTester tester) async {
+   await loadPage(tester);
+
+   emailErrorController.add(null);
+
+   await tester.pump();
+
+   expect(
+     find.descendant(
+         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
+     findsOneWidget,
+   );
+  });
+
+  testWidgets('Should present no error if email is valid', (WidgetTester tester) async {
+   await loadPage(tester);
+
+   emailErrorController.add('');
+   await tester.pump();
+
+   expect(
+     find.descendant(
+         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
+     findsOneWidget,
+   );
   });
 }
