1109a8bf04aae022c6c15ed8c0056ec676db098d
Luis <luis.henrique8000@gmail.com>
2021-04-05 00:10:02 -0300

    ensure LoginPage calls validate when form changes

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:for_dev/ui/components/components.dart';
+import 'login_presenter.dart';
 
 class LoginPage extends StatelessWidget {
+  final LoginPresenter presenter;
+
+ LoginPage(this.presenter);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +31,7 @@ class LoginPage extends StatelessWidget {
                         ),
                       ),
                       keyboardType: TextInputType.emailAddress,
+                      onChanged: presenter.validateEmail,
                     ),
                     Padding(
                       padding: EdgeInsets.only(top: 8, bottom: 32),
@@ -38,6 +43,7 @@ class LoginPage extends StatelessWidget {
                             color: Theme.of(context).primaryColorLight,
                           ),
                         ),
+                        onChanged: presenter.validatePassword,
                         obscureText: true,
                       ),
                     ),
