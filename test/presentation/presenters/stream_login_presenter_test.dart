74c0a2ef75b5364afe7f2892ac356b5ccedd8791
Luis <luis.henrique8000@gmail.com>
2021-04-08 14:29:24 -0300

    ensure EmailErrorStream will not emit duplicated event

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,48 +1,42 @@
-
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:for_dev/presentation/presenters/stream_login_presenter.dart';
 import 'package:for_dev/presentation/protocols/validation.dart';
 import 'package:mockito/mockito.dart';
 
-
-
-
-
 class ValidationSpy extends Mock implements Validation {}
 
-
-void main(){
+void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
 
-  PostExpectation mockValidationCall(String field)=>
-      when(validation.validate(field:field == null ? anyNamed('field') : field, value: anyNamed('value')));
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
 
-  void mockValidation({String field, String value}){
+  void mockValidation({String field, String value}) {
     mockValidationCall(field).thenReturn(value);
   }
 
-  setUp((){
-     validation = ValidationSpy();
-     sut = StreamLoginPresenter(validation: validation);
-     email = faker.internet.email();
-     mockValidation();
-
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+    mockValidation();
   });
 
-  test('Should call Validation wih correct email', (){
-
+  test('Should call Validation wih correct email', () {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
-  test('Should emit email error if validation fails', (){
-   mockValidation(value:'error');
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
 
-    expectLater(sut.emailErrorStream, emits('error'));
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, 'error')));
 
+    sut.validateEmail(email);
     sut.validateEmail(email);
   });
 }
