679b8ca44e22232ffd6edb689df1da332cccb9c6
Luis <luis.henrique8000@gmail.com>
2021-04-08 15:09:23 -0300

    ensure StreamLoginPresenter calls Authentication with correct values

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,13 +1,16 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:for_dev/domain/usecases/authentication.dart';
 import 'package:for_dev/presentation/presenters/stream_login_presenter.dart';
 import 'package:for_dev/presentation/protocols/validation.dart';
 import 'package:mockito/mockito.dart';
 
 class ValidationSpy extends Mock implements Validation {}
+class AuthenticationSpy extends Mock implements Authentication{}
 
 void main() {
   StreamLoginPresenter sut;
+  AuthenticationSpy authentication;
   ValidationSpy validation;
   String email;
   String password;
@@ -22,7 +25,8 @@ void main() {
 
   setUp(() {
     validation = ValidationSpy();
-    sut = StreamLoginPresenter(validation: validation);
+    authentication = AuthenticationSpy();
+    sut = StreamLoginPresenter(validation: validation, authentication: authentication);
     email = faker.internet.email();
     password = faker.internet.password();
     mockValidation();
@@ -97,5 +101,14 @@ void main() {
     sut.validateEmail(email);
     await Future.delayed(Duration.zero);
     sut.validatePassword(password);
+  });
+
+  test('Should call Authentication with correct values', () async {
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    await sut.auth();
+    
+    verify(authentication.auth(AuthenticationParams(email: email, secret: password))).called(1);
   });
 }
