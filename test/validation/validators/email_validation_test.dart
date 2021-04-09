6d1d995ff8323a023a207550a36afa783b2cbee7
Luis <luis.henrique8000@gmail.com>
2021-04-09 19:21:48 -0300

    ensure EmailValidation returns null if email is valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -20,11 +20,13 @@ void main(){
 
   test('Should return null if email is empty', (){
     expect(sut.validate(''), null);
-
   });
 
   test('Should return null if email is null', (){
     expect(sut.validate(null), null);
+  });
 
+  test('Should return null if email is valid', (){
+    expect(sut.validate('luis.henrique8000@gmail.com'), null);
   });
 }
