ca16cb11a100d1ed4d06d8802753735a49b10f3a
Luis <luis.henrique8000@gmail.com>
2021-04-12 18:57:14 -0300

    add GetXLoginPresenter

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -3,5 +3,5 @@ import 'package:for_dev/ui/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 Widget makeLoginPage() {
-  return LoginPage(makeLoginPresenter());
+  return LoginPage(makeGetxLoginPresenter());
 }
--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,8 +2,15 @@ import 'package:for_dev/main/factories/factories.dart';
 import 'package:for_dev/presentation/presenters/presenters.dart';
 import 'package:for_dev/ui/pages/login/login_presenter.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+      validation: makeLoginValidation(),
+      authentication: makeRemoteAuthentication()
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
       validation: makeLoginValidation(),
       authentication: makeRemoteAuthentication()
   );
