9f869aa36d1e526f229640d5f98f074aeebcac2b
Luis <luis.henrique8000@gmail.com>
2021-04-13 19:00:03 -0300

    add SaveCurrentAccount use case

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -1,7 +1,10 @@
 import 'package:for_dev/main/factories/factories.dart';
 import 'package:for_dev/ui/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:for_dev/ui/pages/login/login_presenter.dart';
+import 'package:get/get.dart';
 
 Widget makeLoginPage() {
-  return LoginPage(makeGetxLoginPresenter());
+  final presenter = Get.put<LoginPresenter>(makeGetxLoginPresenter());
+  return LoginPage(presenter);
 }
