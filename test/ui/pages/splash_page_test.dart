590f58c72c68f2f3b4a362eeb7f7ca086d5c0569
Luis <luis.henrique8000@gmail.com>
2021-04-16 16:47:56 -0300

    move SplashPage to its own file

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -2,42 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:for_dev/ui/pages/splash/splash.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 
-class SplashPage extends StatelessWidget {
-  final SplashPresenter presenter;
-
-  SplashPage({@required this.presenter});
-  @override
-  Widget build(BuildContext context) {
-    presenter.loadCurrentAccount();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('4Dev'),
-      ),
-      body: Builder(
-        builder: (context){
-          presenter.navigateToStream.listen((page){
-            if (page?.isNotEmpty == true){
-              Get.offAllNamed(page);
-            }
-          });
-
-          return  Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-    );
-  }
-}
-
-abstract class SplashPresenter{
-  Stream<String> get navigateToStream;
-  Future<void> loadCurrentAccount();
-}
 
 class SplashPresenterSpy extends Mock implements SplashPresenter{}
 
