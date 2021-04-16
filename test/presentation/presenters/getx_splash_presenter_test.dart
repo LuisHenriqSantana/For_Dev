fa1dd0b6f2aae9f65d38a447cf407dd24161669e
Luis <luis.henrique8000@gmail.com>
2021-04-16 19:28:37 -0300

    ensure GetxSplashPresenter calls LoadCurrentAccount

---
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -0,0 +1,32 @@
+import 'package:for_dev/domain/usecases/load_current_account.dart';
+import 'package:for_dev/ui/pages/splash/splash.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class GetxSplashPresenter implements SplashPresenter{
+  final LoadCurrentAccount loadCurrentAccount;
+  var _navigateTo = RxString();
+
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+
+  Future<void> checkAccount() async {
+    await loadCurrentAccount.load();
+  }
+}
+
+class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount{}
+
+void main (){
+  test('Should call LoadCurrentAccount', () async {
+    final loadCurrentAccount = LoadCurrentAccountSpy();
+    final sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+
+    await sut.checkAccount();
+
+    verify(loadCurrentAccount.load()).called(1);
+  });
+}
