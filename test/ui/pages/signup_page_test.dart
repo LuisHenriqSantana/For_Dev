517cb92a21cb6df826791e3aae9257eed573fdf1
Luis <luis.henrique8000@gmail.com>
2021-04-28 14:51:07 -0300

    removed unused tests

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -86,41 +86,6 @@ void main() {
     closeStreams();
   });
 
-  testWidgets('Should load with correct initial state',
-      (WidgetTester tester) async {
-    await loadPage(tester);
-
-    final nameTextChildren = find.descendant(
-        of: find.bySemanticsLabel('Nome'), matching: find.byType(Text));
-    expect(
-      nameTextChildren,
-      findsOneWidget,
-    );
-
-    final emailTextChildren = find.descendant(
-        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-    );
-    final passwordTextChildren = find.descendant(
-        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
-    expect(
-      passwordTextChildren,
-      findsOneWidget,
-    );
-    final passwordConfirmationTextChildren = find.descendant(
-        of: find.bySemanticsLabel('Confirmar senha'),
-        matching: find.byType(Text));
-    expect(
-      passwordConfirmationTextChildren,
-      findsOneWidget,
-    );
-    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-    expect(button.onPressed, null);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
-
   testWidgets('Should call validate with correct values',
       (WidgetTester tester) async {
     await loadPage(tester);
