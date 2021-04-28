9b90de90674ab182361676026b95f13c312e01db
Luis <luis.henrique8000@gmail.com>
2021-04-28 19:37:29 -0300

    move SUT creation to the setup method

--- test/presentation/presenters/getx_surveys_presenter.dart
+++ test/presentation/presenters/getx_surveys_presenter.dart
@@ -3,23 +3,28 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-class GetxSurveysPresenter{
+class GetxSurveysPresenter {
   final LoadSurveys loadSurveys;
 
   GetxSurveysPresenter({@required this.loadSurveys});
 
-  Future<void> loadData()async{
+  Future<void> loadData() async {
     await loadSurveys.load();
   }
 }
 
-class LoadSurveysSpy extends Mock implements LoadSurveys{}
+class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
-void main(){
-  test('Should call LoadSurveys on loadData', () async{
-    final loadSurveys = LoadSurveysSpy();
-    final sut = GetxSurveysPresenter(loadSurveys: loadSurveys);
+void main() {
+  GetxSurveysPresenter sut;
+  LoadSurveysSpy loadSurveys;
 
+  setUp(() {
+    loadSurveys = LoadSurveysSpy();
+    sut = GetxSurveysPresenter(loadSurveys: loadSurveys);
+  });
+
+  test('Should call LoadSurveys on loadData', () async {
     await sut.loadData();
 
     verify(loadSurveys.load()).called(1);
