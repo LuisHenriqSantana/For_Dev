40e1f39aefdf5452586e671b51f0dbb2ba6649dd
Luis <luis.henrique8000@gmail.com>
2021-04-27 22:35:06 -0300

    move RemoteLoadSurveys to its own file

---
+++ lib/data/usecases/load_surveys/remote_load_surveys.dart
@@ -0,0 +1,26 @@
+import 'package:for_dev/data/http/http.dart';
+import 'package:for_dev/data/models/models.dart';
+import 'package:for_dev/domain/entities/entities.dart';
+import 'package:for_dev/domain/helpers/helpers.dart';
+import 'package:for_dev/domain/usecases/usecases.dart';
+import 'package:meta/meta.dart';
+
+class RemoteLoadSurveys implements LoadSurveys {
+  final String url;
+  final HttpClient<List<Map>> httpClient;
+
+  RemoteLoadSurveys({@required this.url, @required this.httpClient});
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final httpResponse = await httpClient.request(url: url, method: 'get');
+      return httpResponse
+          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
+          .toList();
+    } on HttpError catch (error) {
+      throw error == HttpError.forbidden
+          ? DomainError.accessDenied
+          : DomainError.unexpected;
+    }
+  }
+}
