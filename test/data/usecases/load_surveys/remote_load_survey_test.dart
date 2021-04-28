b0d567ac63aade92ebb96ac9ec68563e99b84709
Luis <luis.henrique8000@gmail.com>
2021-04-27 22:24:31 -0300

    ensure RemoteLoadSurveys throws UnexpectedError on 404

--- test/data/usecases/load_surveys/remote_load_survey_test.dart
+++ test/data/usecases/load_surveys/remote_load_survey_test.dart
@@ -56,6 +56,10 @@ void main() {
     mockRequest().thenAnswer((_) async => data);
   }
 
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     url = faker.internet.httpsUrl();
     httpClient = HttpClientSpy();
@@ -95,4 +99,12 @@ void main() {
 
       expect(future, throwsA(DomainError.unexpected));
     });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
