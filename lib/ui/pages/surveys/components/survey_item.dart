61ce8a0afc9f61dc9a47683546deb4e210fce0ff
Luis <luis.henrique8000@gmail.com>
2021-04-28 18:38:29 -0300

    ensure SurveysPage present list if loadSurveys succeeds

--- lib/ui/pages/surveys/components/survey_item.dart
+++ lib/ui/pages/surveys/components/survey_item.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
-
+import 'package:for_dev/ui/pages/pages.dart';
 
 class SurveyItem extends StatelessWidget {
+  final SurveyViewModel viewModel;
+
+  SurveyItem(this.viewModel);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 5),
       child: Container(
         padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(color: Theme.of(context).secondaryHeaderColor,
+        decoration: BoxDecoration(
+            color: Theme.of(context).secondaryHeaderColor,
             boxShadow: [
               BoxShadow(
                   offset: Offset(0, 1),
                   spreadRadius: 0,
                   blurRadius: 2,
-                  color: Colors.black
-              )
+                  color: Colors.black)
             ],
-            borderRadius: BorderRadius.all(Radius.circular(10))
-        ),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "20 ago 2021",
-              style: TextStyle(color: Colors.white, fontSize:20, fontWeight: FontWeight.bold),
+              viewModel.date,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            Text("Qual seu framework web favorito",
-              style: TextStyle(color: Colors.white, fontSize:24),)
+            Text(
+              viewModel.question,
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            )
           ],
         ),
       ),
