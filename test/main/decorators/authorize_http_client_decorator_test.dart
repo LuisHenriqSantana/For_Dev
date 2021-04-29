48bb4fc8820ce8051264cf286f92c670dff3b066
Luis <luis.henrique8000@gmail.com>
2021-04-29 11:43:12 -0300

    add params to request

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,31 +1,48 @@
+import 'package:faker/faker.dart';
 import 'package:for_dev/data/cache/cache.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-class AuthorizeHttpClientDecorator{
+class AuthorizeHttpClientDecorator {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
 
   AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
-  Future<void> request() async{
+
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+    Map headers,
+  }) async {
     await fetchSecureCacheStorage.fetchSecure('token');
   }
 }
 
-class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage{}
+class FetchSecureCacheStorageSpy extends Mock
+    implements FetchSecureCacheStorage {}
 
-void main (){
+void main() {
   AuthorizeHttpClientDecorator sut;
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  String url;
+  String method;
+  Map body;
 
-  setUp((){
-     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
-     sut = AuthorizeHttpClientDecorator(fetchSecureCacheStorage: fetchSecureCacheStorage);
+
+  setUp(() {
+
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    sut = AuthorizeHttpClientDecorator(
+      fetchSecureCacheStorage: fetchSecureCacheStorage,
+    );
+    url = faker.internet.httpUrl();
+    method = faker.randomGenerator.string(10);
+    body = {'any_key': 'any_value'};
   });
 
-  test('Should call FetchSecureCacheStorage with correct key', ()async{
-
-    await sut.request();
+  test('Should call FetchSecureCacheStorage with correct key', () async {
+    await sut.request(url: url, method: method, body: body);
 
     verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
   });
