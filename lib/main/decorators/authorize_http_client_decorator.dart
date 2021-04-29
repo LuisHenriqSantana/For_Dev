c4e0863cf95ca23598f0e1b6b7afd4d47b87a999
Luis <luis.henrique8000@gmail.com>
2021-04-29 15:44:20 -0300

    move AuthorizeHttpClientDecorator to its own file

---
+++ lib/main/decorators/authorize_http_client_decorator.dart
@@ -0,0 +1,30 @@
+import 'package:for_dev/data/cache/cache.dart';
+import 'package:for_dev/data/http/http.dart';
+import 'package:meta/meta.dart';
+
+class AuthorizeHttpClientDecorator implements HttpClient {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+  final HttpClient decoratee;
+
+  AuthorizeHttpClientDecorator(
+      {@required this.fetchSecureCacheStorage, @required this.decoratee});
+
+  Future<dynamic> request({
+    @required String url,
+    @required String method,
+    Map body,
+    Map headers,
+  }) async {
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      final authorizedHeaders = headers ?? {}
+        ..addAll({'x-access-token': token});
+      return await decoratee.request(
+          url: url, method: method, body: body, headers: authorizedHeaders);
+    } on HttpError {
+      rethrow;
+    } catch (error) {
+      throw HttpError.forbidden;
+    }
+  }
+}
