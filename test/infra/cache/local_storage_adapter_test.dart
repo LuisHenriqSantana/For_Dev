a079ef4983a66b30581df1a071337054c3121d85
Luis <luis.henrique8000@gmail.com>
2021-05-01 17:10:55 -0300

    ensure LocalStorageAdapter throws if deleteItem throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -4,7 +4,7 @@ import 'package:meta/meta.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 
-class LocalStorageAdapter{
+class LocalStorageAdapter {
   final LocalStorage localStorage;
 
   LocalStorageAdapter({@required this.localStorage});
@@ -15,7 +15,7 @@ class LocalStorageAdapter{
   }
 }
 
-class LocalStorageSpy extends Mock implements LocalStorage{}
+class LocalStorageSpy extends Mock implements LocalStorage {}
 
 void main() {
   LocalStorageAdapter sut;
@@ -23,17 +23,28 @@ void main() {
   String key;
   dynamic value;
 
-  setUpAll((){
-     key = faker.randomGenerator.string(5);
-     value = faker.randomGenerator.string(50);
-     localStorage = LocalStorageSpy();
-     sut = LocalStorageAdapter(localStorage: localStorage);
+  void mockDeleteItemError() =>
+      when(localStorage.deleteItem(any)).thenThrow(Exception());
 
+  setUpAll(() {
+    key = faker.randomGenerator.string(5);
+    value = faker.randomGenerator.string(50);
+    localStorage = LocalStorageSpy();
+    sut = LocalStorageAdapter(localStorage: localStorage);
   });
-  test('Should call localStorage with correct values',() async{
+
+  test('Should call localStorage with correct values', () async {
     await sut.save(key: key, value: value);
 
     verify(localStorage.deleteItem(key)).called(1);
     verify(localStorage.setItem(key, value)).called(1);
+  });
+
+  test('Should throw if deleteItem throws', () async {
+    mockDeleteItemError();
+
+    final future = sut.save(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
