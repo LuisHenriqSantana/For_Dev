68aa3cb73aa32c01493d2cc86c28aa7f74c37be4
Luis <luis.henrique8000@gmail.com>
2021-05-01 17:43:42 -0300

    ensure LocalStorageAdapter calls fetch with correct value

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -68,4 +68,13 @@ void main() {
       expect(future, throwsA(TypeMatcher<Exception>()));
     });
   });
+  
+  group('fetch', (){
+
+    test('Should call localStorage with correct value', () async{
+      await sut.fetch(key);
+
+      verify(localStorage.getItem(key)).called(1);
+    });
+  });
 }
