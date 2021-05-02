bd17ed844e8fce508361e18fc6b9256e82817f9c
Luis <luis.henrique8000@gmail.com>
2021-05-02 18:50:58 -0300

    move SurveyItems Component to its own file

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:for_dev/ui/components/components.dart';
 import 'package:for_dev/ui/helpers/helpers.dart';
@@ -32,21 +31,13 @@ class SurveysPage extends StatelessWidget {
             stream: presenter.surveysStream,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return ReloadScreen(error: snapshot.error, reload: presenter.loadData,);
+                return ReloadScreen(
+                  error: snapshot.error,
+                  reload: presenter.loadData,
+                );
               }
               if (snapshot.hasData) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      enlargeCenterPage: true,
-                      aspectRatio: 1,
-                    ),
-                    items: snapshot.data
-                        .map((viewModel) => SurveyItem(viewModel))
-                        .toList(),
-                  ),
-                );
+                return SurveyItems(snapshot.data);
               }
               return SizedBox(
                 height: 0,
