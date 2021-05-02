46d10a3b524ad8baad524bef21d20a6d8946680c
Luis <luis.henrique8000@gmail.com>
2021-05-02 01:56:36 -0300

    move RemoteLoadSurveysWithLocalFallback to its own file

---
+++ lib/main/composities/remote_load_surveys_with_local_fallback.dart
@@ -0,0 +1,29 @@
+import 'package:for_dev/data/usecases/usecases.dart';
+import 'package:for_dev/domain/entities/entities.dart';
+import 'package:for_dev/domain/helpers/helpers.dart';
+import 'package:for_dev/domain/usecases/usecases.dart';
+import 'package:meta/meta.dart';
+
+class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
+  final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
+
+  RemoteLoadSurveysWithLocalFallback({
+    @required this.remote,
+    @required this.local,
+  });
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final surveys = await remote.load();
+      await local.save(surveys);
+      return surveys;
+    } catch (error) {
+      if(error == DomainError.accessDenied){
+        rethrow;
+      }
+      await local.validate();
+      return await local.load();
+    }
+  }
+}
--- test/main/composities/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composities/remote_load_surveys_with_local_fallback_test.dart
@@ -2,34 +2,10 @@ import 'package:faker/faker.dart';
 import 'package:for_dev/data/usecases/usecases.dart';
 import 'package:for_dev/domain/entities/entities.dart';
 import 'package:for_dev/domain/helpers/helpers.dart';
-import 'package:for_dev/domain/usecases/usecases.dart';
+import 'package:for_dev/main/composities/composities.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
-class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
-  final RemoteLoadSurveys remote;
-  final LocalLoadSurveys local;
-
-  RemoteLoadSurveysWithLocalFallback({
-    @required this.remote,
-    @required this.local,
-  });
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final surveys = await remote.load();
-      await local.save(surveys);
-      return surveys;
-    } catch (error) {
-      if(error == DomainError.accessDenied){
-        rethrow;
-      }
-      await local.validate();
-      return await local.load();
-    }
-  }
-}
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
