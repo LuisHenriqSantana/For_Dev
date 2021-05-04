32bd41e4fcc1b81d6f8fbc9b1ea7eec3de59430f
Luis <luis.henrique8000@gmail.com>
2021-05-03 22:59:16 -0300

    fixed bug on SplashPage

--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -4,16 +4,17 @@ import 'package:for_dev/ui/mixins/mixins.dart';
 
 import 'splash.dart';
 
-class SplashPage extends StatelessWidget with NavigationManager{
+class SplashPage extends StatelessWidget with NavigationManager {
   final SplashPresenter presenter;
 
   SplashPage({@required this.presenter});
+
   @override
   Widget build(BuildContext context) {
+    presenter.checkAccount();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('4Dev'),
-      ),
+      appBar: AppBar(title: Text('4Dev')),
       body: Builder(
         builder: (context) {
           handleNavigation(presenter.navigateToStream, clear: true);
