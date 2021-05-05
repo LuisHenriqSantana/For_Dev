96ced6a558e96c33030f0ac8386cf411c198cfaf
Luis <luis.henrique8000@gmail.com>
2021-05-05 18:44:02 -0300

    remove duplicated code

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -1,3 +1,4 @@
+import 'package:for_dev/domain/entities/entities.dart';
 import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:for_dev/domain/usecases/usecases.dart';
 import 'package:for_dev/presentation/mixins/mixins.dart';
@@ -23,41 +24,20 @@ class GetxSurveyResultPresenter extends GetxController
   });
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult =
-          await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers
-            .map((answer) => SurveyAnswerViewModel(
-                image: answer.image,
-                answer: answer.answer,
-                percent: '${answer.percent}',
-                isCurrentAnswer: answer.isCurrentAnswer))
-            .toList(),
-      );
-    } on DomainError catch (error) {
-      if (error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
   Future<void>save({@required String answer}) async  {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async  {
     try {
       isLoading = true;
-      final surveyResult =
-      await saveSurveyResult.save(answer: answer);
+      final surveyResult = await action();
       _surveyResult.value = SurveyResultViewModel(
         surveyId: surveyResult.surveyId,
         question: surveyResult.question,
-        answers: surveyResult.answers
-            .map((answer) =>
+        answers: surveyResult.answers.map((answer) =>
             SurveyAnswerViewModel(
                 image: answer.image,
                 answer: answer.answer,
