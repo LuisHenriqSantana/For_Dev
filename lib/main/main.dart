f019f0fe3125c83f4b925daf89f3661ca134059d
Luis <luis.henrique8000@gmail.com>
2021-05-05 05:56:45 -0300

    remove unused param

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -22,9 +22,13 @@ class App extends StatelessWidget {
       initialRoute: '/login',
       getPages: [
         GetPage(name: '/', page: makeSplashPage, transition: Transition.fade),
-        GetPage(name: '/login', page: makeLoginPage, transition: Transition.fadeIn),
+        GetPage(
+            name: '/login', page: makeLoginPage, transition: Transition.fadeIn),
         GetPage(name: '/signup', page: makeSignUpPage),
-        GetPage(name: '/surveys', page: makeSurveysPage, transition: Transition.fadeIn),
+        GetPage(
+            name: '/surveys',
+            page: makeSurveysPage,
+            transition: Transition.fadeIn),
         GetPage(name: '/survey_result/:survey_id', page: makeSurveyResultPage),
       ],
     );
