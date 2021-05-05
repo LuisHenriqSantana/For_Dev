bb78f0daf563242499983ac9f87ebf108339e0cd
Luis <luis.henrique8000@gmail.com>
2021-05-05 18:28:16 -0300

    ensure GetxSurveyResultPresenter emits correct events on success

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -49,6 +49,21 @@ class GetxSurveyResultPresenter extends GetxController
     }
   }
   Future<void>save({@required String answer}) async  {
+    isLoading = true;
+    final surveyResult =
     await saveSurveyResult.save(answer: answer);
+    _surveyResult.value = SurveyResultViewModel(
+      surveyId: surveyResult.surveyId,
+      question: surveyResult.question,
+      answers: surveyResult.answers
+          .map((answer) => SurveyAnswerViewModel(
+          image: answer.image,
+          answer: answer.answer,
+          percent: '${answer.percent}',
+          isCurrentAnswer: answer.isCurrentAnswer))
+          .toList(),
+    );
+    isLoading = false;
+
   }
 }
